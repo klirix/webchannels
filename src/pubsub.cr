48a4a54b59a25ca38aef301c1b9c2739922b1a09
Askhat Saiapov <askhat.saiapov@gmail.com>
2021-03-23 16:49:39 +0300

    mova pubsub into one file

--- src/pubsub.cr
+++ src/pubsub.cr
@@ -3,6 +3,16 @@
 module PubSub(Data)
 
   # :nodoc:
+  record SubscriptionMessage(Data), topic : String, channel : Channel(Data)
+
+  # :nodoc:
+  record UnsubscriptionMessage(Data), topic : String, channel : Channel(Data)
+
+  # :nodoc:
+  record DataMessage(Data), topic : String?, data : Data
+
+
+  # :nodoc:
   class InvalidCommandException < Exception
   end
 
@@ -46,5 +56,44 @@ module PubSub(Data)
   # Broadcasts data to all subscribers
   def broadcast(data : Data)
     @channel.send(DataMessage.new(nil, data))
+  end
+
+  @clients = {} of Channel(Data) => Set(String)
+  @subscriptions = {} of String => Set(Channel(Data))
+  @channel = Channel(SubscriptionMessage(Data) | UnsubscriptionMessage(Data) | DataMessage(Data)).new
+
+  # :nodoc:
+  def subscribe_impl(topic : String, channel : Channel(Data))
+    unless @subscriptions[topic]?
+      @subscriptions[topic] = Set(Channel(Data)).new
+    end
+    @subscriptions[topic] << channel
+    unless @clients[channel]?
+      @clients[channel] = Set(String).new
+    end
+    @clients[channel] << topic
+  end
+
+  # :nodoc:
+  def unsubscribe_impl(topic : String, channel : Channel(Data))
+    if set = @subscriptions[topic]?
+      set.delete channel
+      @clients[channel].delete topic
+      if @clients[channel].size == 0
+        @clients.delete channel
+      end
+    end
+  end
+
+  # :nodoc:
+  def publish_impl(topic : String, data : Data)
+    if set = @subscriptions[topic]?
+      set.each &.send(data)
+    end
+  end
+
+  # :nodoc:
+  def broadcast_impl(data : Data)
+    @clients.each_key &.send(data)
   end
 end
